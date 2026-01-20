236759a3b2deb73c9543d195718269d371b40840
Mudassirburki <mudassirburki762@gmail.com>
2026-01-20 11:09:55 +0500

    Chat and settings UI done

--- src/components/CusttomTabs.jsx
+++ src/components/CusttomTabs.jsx
@@ -1,16 +1,23 @@
 import { StyleSheet, TouchableOpacity, View } from "react-native";
-import { FontAwesome, Ionicons } from '@expo/vector-icons';
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
 import AppText from "./AppText";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function CustomTabs({ state, descriptors, navigation }) {
+  const insets = useSafeAreaInsets();
   const icons = {
     Home: "home",
     Chat: "chatbubbles-outline",
     Settings: "settings",
-    Profile: "person"
+    Profile: "person",
   };
   return (
-    <View style={styles.container}>
+    <View
+      style={[
+        styles.container,
+        { paddingBottom: insets.bottom + 10, height: 60 + insets.bottom },
+      ]}
+    >
       {state.routes.map((route, index) => {
         // Ensure isFocused is always boolean
         const isFocused = state.index === index;
@@ -27,8 +34,12 @@ export default function CustomTabs({ state, descriptors, navigation }) {
             activeOpacity={1} // boolean safe
             accessibilityState={{ selected: !!accessibilitySelected }} // cast to boolean
           >
-            <Ionicons name={icons[route.name]} size={24} color={isFocused ? "#BB4100" : "#999"} />
-            <AppText.small style={{ color: isFocused ? '#BB4100' : '#999' }}>
+            <Ionicons
+              name={icons[route.name]}
+              size={24}
+              color={isFocused ? "#BB4100" : "#999"}
+            />
+            <AppText.small style={{ color: isFocused ? "#BB4100" : "#999" }}>
               {route.name}
             </AppText.small>
           </TouchableOpacity>
@@ -39,15 +50,19 @@ export default function CustomTabs({ state, descriptors, navigation }) {
 }
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
+    flexDirection: "row",
     height: 60,
     borderTopWidth: 1,
-    borderTopColor: '#ddd',
-    backgroundColor: '#fff',
+    borderTopColor: "#ddd",
+    backgroundColor: "#fff",
+     position: 'absolute', // float at bottom
+    left: 0,
+    right: 0,
+    bottom: 0,
   },
   tab: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
